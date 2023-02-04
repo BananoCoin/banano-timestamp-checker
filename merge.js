35a9e10a8f5bcb327e3a3f86fc74fee3e78871be
coranos <coranos@users.noreply.github.com>
2023-02-03 20:05:02 -0600

    updating dependencies

--- merge.js
+++ merge.js
@@ -1,85 +1,99 @@
 'use strict';
 const fs = require('fs');
-const path = require('path');
+const readline = require('readline');
 
-const VERBOSE = true;
+const DEBUG = false;
 
 const getDate = (ts) => {
-  const tsInt = parseInt(ts, 10)*1000;
-  return new Date(tsInt).toISOString();
+  // console.log('getDate', ts);
+  return new Date(parseInt(ts, 10)*1000).toISOString().substring(0, 10);
 };
 
 const run = async () => {
-  console.log('banano-distribution-stats merge');
-  if (process.argv.length < 3) {
+  console.log('banano-timestamp-checker');
+
+  if (process.argv.length < 5) {
     console.log('#usage:');
-    console.log('npm run merge <timestamp-in-file> <timestamp-out-file>');
-  } else {
-    const timestampInFileNm = process.argv[2];
-    const timestampOutFileNm = process.argv[3];
-    console.log('timestampInFileNm', timestampInFileNm);
-    console.log('timestampOutFileNm', timestampOutFileNm);
-
-
-    const clearLines = () => {
-      const outFilePtr = fs.openSync(timestampOutFileNm, 'w');
-      fs.closeSync(outFilePtr);
-    };
-
-    const appendLine = (line) => {
-      const outFilePtr = fs.openSync(timestampOutFileNm, 'a');
-      fs.writeSync(outFilePtr, line);
-      fs.writeSync(outFilePtr, '\n');
-      fs.closeSync(outFilePtr);
-    };
-
-    const timestampCacheByHashMap = {};
-    timestampCacheByHashMap.set = (hash, timestamp) => {
-      if (!fs.existsSync('data')) {
-        fs.mkdirSync('data', {recursive: true});
-      }
-      const fileNm = path.join('data', hash);
-      const filePtr = fs.openSync(fileNm, 'w');
-      fs.writeSync(filePtr, timestamp);
-      fs.closeSync(filePtr);
-    };
-    timestampCacheByHashMap.size = () => {
-      if (fs.existsSync('data')) {
-        return fs.readdirSync('data').length;
-      }
-      return 0;
-    };
-    timestampCacheByHashMap.has = (hash) => {
-      const fileNm = path.join('data', hash);
-      return fs.existsSync(fileNm);
-    };
-    timestampCacheByHashMap.get = (hash) => {
-      const fileNm = path.join('data', hash);
-      if (fs.existsSync(fileNm)) {
-        return fs.readFileSync(fileNm, {encoding: 'UTF-8'});
-      }
-    };
-
-    clearLines();
-
-    const timestampsStr = fs.readFileSync(timestampInFileNm, {encoding: 'UTF-8'});
-    const timestampLines = timestampsStr.split('\n');
-    for (const timestampLine of timestampLines) {
-      if (timestampLine.length !== 0) {
-        const timestampData = timestampLine.split(',');
-        const hash = timestampData[0];
-        const oldTimestamp = timestampData[1];
-        if (timestampCacheByHashMap.has(hash)) {
-          const newTimestamp = BigInt(timestampCacheByHashMap.get(hash));
-          if (VERBOSE) {
-            console.log(hash, oldTimestamp, '=>', newTimestamp, getDate(newTimestamp));
-          }
-          const newTimestampLine = `${hash},${newTimestamp.toString()}`;
-          appendLine(newTimestampLine);
-        }
-      }
-    }
+    console.log('npm run merge <merge-date> <before-file> <at-and-after-file> <outfile>');
+    return;
   }
+
+  const mergeDate = process.argv[2];
+  const beforeFileName = process.argv[3];
+  const atAndAfterFileName = process.argv[4];
+  const outFileName = process.argv[5];
+
+  console.log(`mergeDate:'${mergeDate}'`);
+  console.log(`beforeFileName:'${beforeFileName}'`);
+  console.log(`atAndAfterFileName:'${atAndAfterFileName}'`);
+  console.log(`outFileName:'${outFileName}'`);
+
+  const outStream = fs.createWriteStream(outFileName);
+
+  const beforeFn = (timestamp) => {
+    const date = getDate(timestamp);
+    const flag = date.localeCompare(mergeDate) < 0;
+    // console.log('beforeFn', timestamp, date, '<', mergeDate, '=', flag);
+    return flag;
+  };
+
+  const atAndAfterFn = (timestamp) => {
+    const date = getDate(timestamp);
+    const flag = date.localeCompare(mergeDate) >= 0;
+    // console.log('atAndAfterFn', timestamp, date, '>=', mergeDate, '=', flag);
+    return flag;
+  };
+
+  const processStream = async (fileNm, timestampFn) => {
+    let time = 0;
+    let lineIx = 0n;
+    let skippedLineCount = 0n;
+    let writtenLineCount = 0n;
+    let logCount = 0;
+    const stream = fs.createReadStream(fileNm);
+    const lines = readline.createInterface({
+      input: stream,
+      crlfDelay: Infinity,
+    });
+
+    for await (const line of lines) {
+      // console.log('timestampLine', lineIx);
+      try {
+        if (Date.now() > time + 10000) {
+          logCount++;
+          console.log(fileNm, new Date(Date.now()).toISOString(), lineIx, 'skipped', skippedLineCount, 'written', writtenLineCount);
+          time = Date.now();
+        }
+        if (DEBUG) {
+          if (logCount > 2) {
+            return;
+          }
+        }
+        if (line.length !== 0) {
+          const timestampData = line.split(',');
+          const hash = timestampData[0];
+          const timestamp = timestampData[1];
+          if (timestampFn(timestamp)) {
+            outStream.write(hash);
+            outStream.write(',');
+            outStream.write(timestamp);
+            outStream.write('\n');
+            writtenLineCount++;
+          } else {
+            skippedLineCount++;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+      lineIx++;
+    }
+  };
+  await processStream(beforeFileName, beforeFn);
+  await processStream(atAndAfterFileName, atAndAfterFn);
+
+  outStream.end();
 };
 
 const runOrError = async () => {
